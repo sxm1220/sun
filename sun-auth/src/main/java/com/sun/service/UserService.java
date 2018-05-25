05691f877c3b5c5c82d8980383aaa0d0e297d5d9
sxm1220 <565268126@qq.com>
2018-05-25 17:46:59 +0800

    项目初始导入

---
+++ sun-auth/src/main/java/com/sun/service/UserService.java
@@ -0,0 +1,13 @@
+package com.sun.service;
+
+
+import com.sun.domain.User;
+
+/**
+ * Created by SuperS on 2017/9/25.
+ *
+ * @author SuperS
+ */
+public interface UserService {
+    void createUser(User user);
+}
--- sun-zuul/src/main/java/com/sun/SunZuulApplication.java
+++ sun-zuul/src/main/java/com/sun/SunZuulApplication.java
@@ -2,12 +2,14 @@ package com.sun;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.oauth2.client.EnableOAuth2Sso;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 
 @SpringBootApplication
 @EnableEurekaClient
 @EnableZuulProxy
+@EnableOAuth2Sso
 public class SunZuulApplication {
 
 	public static void main(String[] args) {
